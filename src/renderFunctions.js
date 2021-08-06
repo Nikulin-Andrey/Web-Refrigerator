8e6d616366deb9119fc29863f0989b7645b0e5c1
Nikulin Andrey <andrey_nikulin3@mail.ru>
2021-08-06 18:59:34 +0300

    update recipes style

--- src/renderFunctions.js
+++ src/renderFunctions.js
@@ -1,24 +1,33 @@
 function renderRecipes(recipes, allIngredients) {
     const container = document.getElementById('recipes_container');
     if (recipes.length > 0) {
-        recipes.forEach(function (recipe) {
+        recipes.forEach(function (recipe, index) {
             container.insertAdjacentHTML('beforeend', `
         <div class="recipe">
-            <h2>${recipe.name}</h2>
-            <div class="info">
-                <img src="${recipe.img}">
-                <div class="ingredients_info">
-                    <div class="ingredients_container">
-                        <h3>Ингредиенты:</h3>
-                        <ul></ul>
+            <div class="recipe_exit">
+                <img src="img/arrow.svg" alt="exit_recipe" class="arrow">
+            </div>
+            <div class="recipe_info">
+                <div class="recipe_info_left_content">
+                    <h3>${recipe.name}</h3>
+                    <div class="recipe_image_container">
+                        <img src="${recipe.img}">
                     </div>
+                    <div class="ingredients_info">
+                        <div id="ingredients_container_${index}">
+                            <h3>Ингредиенты:</h3>
+                            <ul></ul>
+                        </div>
+                    </div>
+                </div>  
+                <div class="recipe_info_right_content">
+                    <h3>Рецепт</h3>
+                    <p>${recipe.description}</p>
                 </div>
-            </div>    
-            <p>${recipe.description}</p>
+            </div> 
         </div>
         `);
-            const ingredientsContainer = container.lastElementChild.lastElementChild.
-                previousElementSibling.lastElementChild.lastElementChild;
+            const ingredientsContainer = document.getElementById(`ingredients_container_${index}`);
             recipe.ingredients.forEach(recipeIngredient => {
                 const rightIngredient = allIngredients.find(ingredient => ingredient.id === recipeIngredient.id);
                 ingredientsContainer.lastElementChild.insertAdjacentHTML('beforeend', `
@@ -27,7 +36,7 @@ function renderRecipes(recipes, allIngredients) {
             });
         });
     } else {
-        container.innerHTML =  '<h3>Ничего не найдено</h3>'
+        container.innerHTML = '<h3>Ничего не найдено</h3>'
     }
 }
 function renderDropDownIngredientList(ingredients) {
@@ -61,4 +70,4 @@ function renderDayRecipes(recipe) {
         }
     }
 }
-export {renderRecipes, renderDropDownIngredientList, renderDayRecipes};
+export { renderRecipes, renderDropDownIngredientList, renderDayRecipes };
